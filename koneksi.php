328a0fce8eacfebd6e745b17bc29354cdd345160
Vinna Laila <vinnalailaluqiana@gmail.com>
2025-06-21 21:20:40 +0700

    koneksi edit

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,2 @@
 <?php
 $db = new PDO('mysql:host=localhost;dbname=db_petshop','root','');
-
-
-// <?php
-// $db = new PDO('mysql:host=ftpupload.net;dbname=if0_39048545_db_petshop','if0_39048545','vinna1509gf'); -->
-
-// 
